855058745c9e61c75f44b134936a0f52f0280276
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2023-03-29 11:15:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const form = document.getElementById("login-form");
 
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // TODO: Add validation code and login functionality here
+
+  console.log("Username: ", username);
+  console.log("Password: ", password);
+});
